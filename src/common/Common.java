266e50d8dddc8a73b5f2e68453f18f528533f8f7
v_liyichen <v_liyichen@baidu.com>
2020-10-27 19:12:05 +0800

    commit common test

---
+++ src/common/Common.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * @Description
+ * @Author v_liyichen
+ * @date 2020.10.27 19:11
+ */
+public class Common {
+
+    public static void main(String[] args) {
+        System.out.println("common");
+    }
+}
